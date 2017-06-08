71c3c10a13e71f53c3d27fb2beccfd5e5ff3b5dc
KatherineHanson <katherinemariehanson@gmail.com>
2017-06-07 23:26:37 -0700

    Corrected previous submission's bug, added resume and top ten list content to HTML, expanded guessing game functionality

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict';
+// 'use strict';
 
 // Customize greeting to user
 var today = new Date();
@@ -12,53 +12,142 @@ if (hourNow > 18) {
 } else if (hourNow > 0) {
   greeting = 'Good morning!';
 } else {
-  greeting = 'Welcome!'
+  greeting = 'Welcome!';
+  console.log('ERROR: Something went wrong!');
 }
 
-document.write('<h1>' + greeting + '</h1>');
+var greet = document.getElementById('greetingH1');
+greet.textContent = greeting;
 
-// Ask user five yes or no questions about Katherine, with a mix of yes or no answers
+// Get user's name
+var userName = prompt('Here there be dragons. Ye shall slay them by answering seven questions. What is your name, traveler?');
+var response = alert('Excellent, ' + userName + ', we shall begin.');
+
+// Ask user seven yes or no questions about Katherine, with a mix of yes or no answers
+// Count and output number of correct answers
+var correct = [];
+
 var states = prompt('Has Katherine coded in more than one state?');
 if (states.toLowerCase() === 'y' || states.toLowerCase() === 'yes') {
   alert('Correct! Katherine has coded in Washington where she lives, and Florida where her hometown is.');
   console.log('When asked whether Katherine has coded in more than one state, the user correctly guessed yes.');
+  correct.push('Correct!');
 } else {
   alert('Not quite, but good guess! Katherine has coded in Washington where she lives, and Florida where her hometown is.');
   console.log('When asked whether Katherine has coded in more than one state, the user incorrectly did not guess yes.');
 }
 
+console.log('Answers correct so far: ' + correct.length);
+
 var previousJob = prompt('Was Katherine in the tech industry she decided to become a programmer?');
-if (previousJob.toLowerCase() === 'y' || states.toLowerCase() === 'yes') {
+if (previousJob.toLowerCase() === 'y' || previousJob.toLowerCase() === 'yes') {
   alert('Correct! Katherine was once a product manager at a small Seattle-area startup. While there, she discovered an interest in programming.');
   console.log('When asked whether Katherine was in the tech industry before she decided to become a programmer, the user correctly guessed yes.');
+  correct.push('Correct!');
 } else {
   alert('Nope, but again, good guess! Katherine was once a product manager at a small Seattle-area startup. While there, she discovered an interest in programming');
   console.log('When asked whether Katherine was in the tech industry before she decided to become a programmer, the user incorrectly did not guess yes.');
 }
 
-var nerd = prompt('Is Katherine a big nerd for anime and manga?')
+console.log('Answers correct so far: ' + correct.length);
+
+var nerd = prompt('Is Katherine a big nerd for anime and manga?');
 if (nerd.toLowerCase() === 'y' || nerd.toLowerCase() === 'yes') {
   alert('Correct! Katherine has not missed a Sakura-Con since moving to Seattle in January 2014.');
   console.log('When asked whether Katherine is an anime and manga nerd, the user correctly guessed yes.');
+  correct.push('Correct!');
 } else {
   alert('Nope, but good guess! Katherine has not missed a Sakura-Con since moving to Seattle in January 2014.');
   console.log('When asked whether Katherine is an anime and manga nerd, the user incorrectly did not guess yes.');
 }
 
+console.log('Answers correct so far: ' + correct.length);
+
 var cooking = prompt('Is Katherine bad at cooking?');
 if (cooking.toLowerCase() === 'n' || cooking.toLowerCase() === 'no') {
   alert('Correct! Katherine enjoys experimenting with cooking and code alike, and recently learned to make a mean blood orange marmalade.');
   console.log('When asked whether Katherine is bad at cooking, the user correctly guessed no.');
+  correct.push('Correct!');
 } else {
   alert('Not quite! Katherine enjoys experimenting with cooking and code alike, and recently learned to make a mean blood orange marmalade.');
   console.log('When asked whether Katherine is bad at cooking, the user incorrectly did not guess no.');
 }
 
+console.log('Answers correct so far: ' + correct.length);
+
 var rainy = prompt('Is Seattle rainy in the summer?');
 if (rainy.toLowerCase() === 'n' || rainy.toLowerCase() === 'no') {
   alert('Correct! Supposedly. As a humble alert, I cannot go outside and see.');
   console.log('When asked whether Seattle is rainy in the summer, the user correctly guessed no.');
+  correct.push('Correct!');
 } else {
   alert('Incorrect! Seattle summers are amazingggggg. Supposedly. As a humble alert, I cannot go outside and see.');
   console.log('When asked whether Seattle is rainy in the summer, the user incorrectly did not guess no.');
+}
+
+console.log('Answers correct so far: ' + correct.length);
+
+// Take number input
+// Give user four opportunities to get the right answer
+// When user answers too high or low, give 'Too high!' or 'Too low!' message
+var correctNumber = Math.floor((Math.random() * 10) + 1);
+console.log('The answer is ' + correctNumber + '.');
+for (var i = 0; i < 4; i++) {
+  var guessedNumber = parseInt(prompt('Which number am I thinking of? You have four tries.'));
+  if (guessedNumber === correctNumber) {
+    alert('Correct! Great job!');
+    console.log('When asked what the program is thinking, the user correctly guessed ' + guessedNumber + '.');
+    correct.push('Correct!');
+    i += 4;
+  } else if (guessedNumber === '') {
+    alert('You must enter a number!');
+    console.log('When asked what the program is thinking, the user entered nothing.');
+  }
+  else if (guessedNumber < correctNumber) {
+    alert('Too low!');
+    console.log('When asked which number the program is thinking of, the user guessed too low.');
+  } else if (guessedNumber > correctNumber) {
+    alert('Too high!');
+    console.log('When asked which number the program is thinking of, the user guessed too high.');
+  }
+}
+
+console.log('Answers correct so far: ' + correct.length);
+
+// Take user input
+// Give user six opportunities to get the right answer out of multiple possibilites
+var nerdSeries = ['Psycho-Pass', 'Revolutionary Girl Utena', 'Star Trek: Deep Space Nine', 'Psycho Pass', 'Utena', 'Star Trek Deep Space Nine', 'Star Trek: DS9', 'Star Trek DS9', 'DS9'];
+var nerdSeriesCount = nerdSeries.length;
+var i = 0;
+while (i < 6) {
+  var guessedSeries = prompt('Name a favorite geeky TV show of Katherine\'s. You have six tries.');
+  for (var x = 0; x < nerdSeriesCount; x++) {
+    if (guessedSeries.toLowerCase() === nerdSeries[x].toLowerCase()) {
+      alert('Correct! Katherine enjoyed watching ' + guessedSeries + '. The correct answers are: ' + nerdSeries[0] + ', ' + nerdSeries[1] + ', and ' + nerdSeries[2] + '.');
+      console.log('When asked to name a favorite geeky TV show of Katherine\'s, the user correctly guessed ' + guessedSeries + '.');
+      correct.push('Correct!');
+      x += nerdSeriesCount;
+      i += 6;
+    }
+  }
+  if (i < 6) {
+    alert('Not according to my list!');
+    console.log('When asked to name a favorite geeky TV show of Katherine\'s, the user incorrectly guessed ' + guessedSeries + '.');
+    i++;
+  }
+  if (i === 0) {
+    alert('Youre out of guesses!');
+    console.log('The user ran out of guesses before they could guess a favorite geeky TV show of Katherine\'s.');
+    i++;
+  }
+}
+
+console.log('Answers correct so far: ' + correct.length);
+
+var finalAnswerCount = correct.length;
+if (finalAnswerCount === 7) {
+  alert('You got ' + finalAnswerCount + ' out of 7, a perfect score! Here\'s a virtual high-five, O dragon slayer.');
+}
+else {
+  alert('You got ' + finalAnswerCount + ' out of 7. Not perfect, but the dragons here are actually pretty chill. You may proceed.');
 }
