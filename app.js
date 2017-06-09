543ba7ba583e53d9737df9493d5e7473ad193e62
Frederick Thomas <supercodingninja@outlook.com>
2017-06-08 17:04:37 -0700

    q6

--- app.js
+++ app.js
@@ -49,53 +49,61 @@ function question1() {
 console.log('Answers correct so far: ' + correct.length);
 
 // Question 2
-var previousJob = prompt('Was Katherine in the tech industry she decided to become a programmer?');
-if (previousJob.toLowerCase() === 'y' || previousJob.toLowerCase() === 'yes') {
-  alert('Correct! Katherine was once a product manager at a small Seattle-area startup. While there, she discovered an interest in programming.');
-  console.log('When asked whether Katherine was in the tech industry before she decided to become a programmer, the user correctly guessed yes.');
-  correct.push('Correct!');
-} else {
-  alert('Nope, but again, good guess! Katherine was once a product manager at a small Seattle-area startup. While there, she discovered an interest in programming');
-  console.log('When asked whether Katherine was in the tech industry before she decided to become a programmer, the user incorrectly did not guess yes.');
+function question2() {
+  var previousJob = prompt('Was Katherine in the tech industry she decided to become a programmer?');
+  if (previousJob.toLowerCase() === 'y' || previousJob.toLowerCase() === 'yes') {
+    alert('Correct! Katherine was once a product manager at a small Seattle-area startup. While there, she discovered an interest in programming.');
+    console.log('When asked whether Katherine was in the tech industry before she decided to become a programmer, the user correctly guessed yes.');
+    correct.push('Correct!');
+  } else {
+    alert('Nope, but again, good guess! Katherine was once a product manager at a small Seattle-area startup. While there, she discovered an interest in programming');
+    console.log('When asked whether Katherine was in the tech industry before she decided to become a programmer, the user incorrectly did not guess yes.');
+  }
 }
 
 console.log('Answers correct so far: ' + correct.length);
 
 // Question 3
-var nerd = prompt('Is Katherine a big nerd for anime and manga?');
-if (nerd.toLowerCase() === 'y' || nerd.toLowerCase() === 'yes') {
-  alert('Correct! Katherine has not missed a Sakura-Con since moving to Seattle in January 2014.');
-  console.log('When asked whether Katherine is an anime and manga nerd, the user correctly guessed yes.');
-  correct.push('Correct!');
-} else {
-  alert('Nope, but good guess! Katherine has not missed a Sakura-Con since moving to Seattle in January 2014.');
-  console.log('When asked whether Katherine is an anime and manga nerd, the user incorrectly did not guess yes.');
+function question3() {
+  var nerd = prompt('Is Katherine a big nerd for anime and manga?');
+  if (nerd.toLowerCase() === 'y' || nerd.toLowerCase() === 'yes') {
+    alert('Correct! Katherine has not missed a Sakura-Con since moving to Seattle in January 2014.');
+    console.log('When asked whether Katherine is an anime and manga nerd, the user correctly guessed yes.');
+    correct.push('Correct!');
+  } else {
+    alert('Nope, but good guess! Katherine has not missed a Sakura-Con since moving to Seattle in January 2014.');
+    console.log('When asked whether Katherine is an anime and manga nerd, the user incorrectly did not guess yes.');
+  }
 }
 
 console.log('Answers correct so far: ' + correct.length);
 
 // Question 4
-var cooking = prompt('Is Katherine bad at cooking?');
-if (cooking.toLowerCase() === 'n' || cooking.toLowerCase() === 'no') {
-  alert('Correct! Katherine enjoys experimenting with cooking and code alike, and recently learned to make a mean blood orange marmalade.');
-  console.log('When asked whether Katherine is bad at cooking, the user correctly guessed no.');
-  correct.push('Correct!');
-} else {
-  alert('Not quite! Katherine enjoys experimenting with cooking and code alike, and recently learned to make a mean blood orange marmalade.');
-  console.log('When asked whether Katherine is bad at cooking, the user incorrectly did not guess no.');
+function question4() {
+  var cooking = prompt('Is Katherine bad at cooking?');
+  if (cooking.toLowerCase() === 'n' || cooking.toLowerCase() === 'no') {
+    alert('Correct! Katherine enjoys experimenting with cooking and code alike, and recently learned to make a mean blood orange marmalade.');
+    console.log('When asked whether Katherine is bad at cooking, the user correctly guessed no.');
+    correct.push('Correct!');
+  } else {
+    alert('Not quite! Katherine enjoys experimenting with cooking and code alike, and recently learned to make a mean blood orange marmalade.');
+    console.log('When asked whether Katherine is bad at cooking, the user incorrectly did not guess no.');
+  }
 }
 
 console.log('Answers correct so far: ' + correct.length);
 
 // Question 5
-var rainy = prompt('Is Seattle rainy in the summer?');
-if (rainy.toLowerCase() === 'n' || rainy.toLowerCase() === 'no') {
-  alert('Correct! Supposedly. As a humble alert, I cannot go outside and see.');
-  console.log('When asked whether Seattle is rainy in the summer, the user correctly guessed no.');
-  correct.push('Correct!');
-} else {
-  alert('Incorrect! Seattle summers are amazingggggg. Supposedly. As a humble alert, I cannot go outside and see.');
-  console.log('When asked whether Seattle is rainy in the summer, the user incorrectly did not guess no.');
+function question5() {
+  var rainy = prompt('Is Seattle rainy in the summer?');
+  if (rainy.toLowerCase() === 'n' || rainy.toLowerCase() === 'no') {
+    alert('Correct! Supposedly. As a humble alert, I cannot go outside and see.');
+    console.log('When asked whether Seattle is rainy in the summer, the user correctly guessed no.');
+    correct.push('Correct!');
+  } else {
+    alert('Incorrect! Seattle summers are amazingggggg. Supposedly. As a humble alert, I cannot go outside and see.');
+    console.log('When asked whether Seattle is rainy in the summer, the user incorrectly did not guess no.');
+  }
 }
 
 console.log('Answers correct so far: ' + correct.length);
@@ -104,25 +112,27 @@ console.log('Answers correct so far: ' + correct.length);
 // Take number input
 // Give user four opportunities to get the right answer
 // When user answers too high or low, give 'Too high!' or 'Too low!' message
-var correctNumber = Math.floor((Math.random() * 10) + 1);
-console.log('The answer is ' + correctNumber + '.');
-for (var i = 0; i < 4; i++) {
-  var guessedNumber = parseInt(prompt('Which number am I thinking of? You have four tries.'));
-  if (guessedNumber === correctNumber) {
-    alert('Correct! Great job!');
-    console.log('When asked what the program is thinking, the user correctly guessed ' + guessedNumber + '.');
-    correct.push('Correct!');
-    i += 4;
-  } else if (isNaN(guessedNumber)) {
-    alert('You must enter a number!');
-    console.log('When asked what the program is thinking, the user entered nothing.');
-  }
-  else if (guessedNumber < correctNumber) {
-    alert('Too low!');
-    console.log('When asked which number the program is thinking of, the user guessed too low.');
-  } else if (guessedNumber > correctNumber) {
-    alert('Too high!');
-    console.log('When asked which number the program is thinking of, the user guessed too high.');
+function question6() {
+  var correctNumber = Math.floor((Math.random() * 10) + 1);
+  console.log('The answer is ' + correctNumber + '.');
+  for (var i = 0; i < 4; i++) {
+    var guessedNumber = parseInt(prompt('Which number am I thinking of? You have four tries.'));
+    if (guessedNumber === correctNumber) {
+      alert('Correct! Great job!');
+      console.log('When asked what the program is thinking, the user correctly guessed ' + guessedNumber + '.');
+      correct.push('Correct!');
+      i += 4;
+    } else if (isNaN(guessedNumber)) {
+      alert('You must enter a number!');
+      console.log('When asked what the program is thinking, the user entered nothing.');
+    }
+    else if (guessedNumber < correctNumber) {
+      alert('Too low!');
+      console.log('When asked which number the program is thinking of, the user guessed too low.');
+    } else if (guessedNumber > correctNumber) {
+      alert('Too high!');
+      console.log('When asked which number the program is thinking of, the user guessed too high.');
+    }
   }
 }
 
